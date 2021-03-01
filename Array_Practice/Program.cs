c47c4ce184b70123c13e7919bb64128d7bd1b9fe
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 14:23:46 -0800

    add an array of grades

--- Array_Practice/Program.cs
+++ Array_Practice/Program.cs
@@ -6,7 +6,17 @@ namespace Array_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //array type int, named grade, and it has space for 5 items in the array
+            int[] grades = new int[5];
+
+            //set value at each index in the array
+            grades[0] = 100;
+            grades[1] = 75;
+            grades[2] = 80;
+            grades[3] = 35;
+            grades[4] = 70;
+
+            Console.WriteLine("Grades at index 0 : {0}", grades[0]);
         }
     }
 }
