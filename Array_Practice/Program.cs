8a0103de2a2e8c31339e2859b831f28c5c9f8829
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 14:31:27 -0800

    set up for loop with an array

--- Array_Practice/Program.cs
+++ Array_Practice/Program.cs
@@ -19,10 +19,10 @@ namespace Array_Practice
 
             Console.WriteLine("Grades at index 0 : {0}", grades[0]);
             // assign value to array grades at index 0 
-            string input = Console.ReadLine();
-            grades[0] = int.Parse(input);
+            //string input = Console.ReadLine();
+            //grades[0] = int.Parse(input);
 
-            Console.WriteLine("Grades at index 0 : {0}", grades[0]);
+            //Console.WriteLine("Grades at index 0 : {0}", grades[0]);
 
             //another way to initialize an array
             int[] gradesMath = { 20, 13, 12, 8, 8, 10 };
@@ -30,7 +30,21 @@ namespace Array_Practice
             //third way to initialize an array
             int[] gradesEnglish = new int[] { 14, 20, 10, 10, 12, 14, 19 };
 
+            //length of array
             Console.WriteLine("Length of grades is " + gradesMath.Length);
+
+            //for loop with array
+            int[] nums = new int[10];
+
+            for(int i = 0; i < 10; i++)
+            {
+                nums[i] = i;
+            }
+
+            for(int j = 0; j <10; j++)
+            {
+                Console.WriteLine("Element{0} = {1}", j, nums[j]);
+            }
         }
     }
 }
